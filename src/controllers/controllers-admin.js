db8a5b589b89e4d4b47c858775d6eceb38599d13
Izzan Abdul <izzanabdul123@gmail.com>
2024-05-07 08:44:20 +0700

    feat: add multer

--- src/controllers/controllers-admin.js
+++ src/controllers/controllers-admin.js
@@ -22,8 +22,8 @@ module.exports = {
   },
   addCarsData: (req, res) => {
     const { name, transmission, price_12, price_24, price_fullday } = req.body;
-    const { image1, image2 } = req.file.path;
-    const sql = 'insert into cars (name, transmission, image1, image2, price_12, price_24, price_fullday, created_at, modified_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    // const { image } = req.file.filename;
+    const sql = 'insert into cars (name, transmission, image, price_12, price_24, price_fullday, created_at, modified_at) values (?, ?, ?, ?, ?, ?, ?, ?)';
 
     if (!req.file) {
       const err = new Error('Image Harus di Upload');
@@ -31,25 +31,38 @@ module.exports = {
       throw err;
     }
 
-    db.query(sql, [name, transmission, image1, image2, price_12, price_24, price_fullday, new Date().toISOString(), new Date().toISOString()], (err, result) => {
+    db.query(sql, [name, transmission, req.file.filename, price_12, price_24, price_fullday, new Date().toISOString(), new Date().toISOString()], (err, result) => {
       if (err) {
         response(500, err, err.message, res);
       }
 
       response(201, result, 'Successfully added cars data', res);
     });
+
+    // res.send(image);
+  },
+  getCarsData: (req, res) => {
+    const sql = 'select * from cars';
+
+    db.query(sql, (err, result) => {
+      if (err) {
+        response(500, err, err.message, res);
+      }
+
+      response(200, result, 'Successfully get cars data', res);
+    });
   },
   editCarsData: (req, res) => {
     const { id, name, transmission, price_12, price_24, price_fullday } = req.body;
     const { image1, image2 } = req.file.path;
-    const sql = 'update cars set name = ?, transmission = ?, image1 = ?, image2 = ?, price_12 = ?, price_24 = ?, price_fullday = ?, modified_at = ? where id = ?';
+    const sql = 'update cars set name = ?, transmission = ?, image = ?, price_12 = ?, price_24 = ?, price_fullday = ?, modified_at = ? where id = ?';
 
     db.query(sql, [name, transmission, image1, image2, price_12, price_24, price_fullday, new Date().toISOString(), id], (err, result) => {
       if (err) {
         response(500, err, err.message, res);
       }
 
-      response(201, result, 'Successfully update cars data', res);
+      response(200, result, 'Successfully update cars data', res);
     });
   },
 };
--- src/routes/route-lib.js
+++ src/routes/route-lib.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const multer = require('multer');
 const { admin } = require('../controllers/index-control');
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'public/cars');
+  },
+  filename: (req, file, callback) => {
+    callback(null, new Date().getTime() + '_' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, callback) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+};
+
+const upload = multer({
+  storage: fileStorage,
+  fileFilter: fileFilter,
+});
+
+router.get('/cars', admin.getCarsData);
 router.post('/admin/roles', admin.addRolesData);
-router.post('/admin/cars', admin.addCarsData);
+router.post('/admin/cars', upload.single('image'), admin.addCarsData);
+// router.patch('/admin/cars', upload.single('image'), admin.editCarsData);
 
 module.exports = router;
