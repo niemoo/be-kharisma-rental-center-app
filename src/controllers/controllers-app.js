286ef9758ac0db96ff2664f40e624f9871fefd1a
Izzan Abdul <izzanabdul123@gmail.com>
2024-06-19 20:07:44 +0700

    feat: controller add booking

--- src/controllers/controllers-app.js
+++ src/controllers/controllers-app.js
@@ -35,33 +35,21 @@ module.exports = {
       const addNewBooking = 'INSERT INTO bookings (user_id, car_id, booking_date) VALUES (?, ?, ?)';
       const addBookingDetail =
         'INSERT INTO booking_details (booking_id, alamat, instagram, tujuan_sewa, rute, jaminan, total_price, tempat_ambil, start_time, end_time, start_date, end_date, status, created_at, modified_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+      const addPayment = 'INSERT INTO payments (booking_id, payment_status) VALUES (?, ?)';
       const dateFormat = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
       db.query(addNewBooking, [user_id, car_id, dateFormat], (err, bookingResult) => {
         if (err) throw err;
 
-        response(201, bookingResult, 'Successfully add new booking', res);
-        const booking_id = bookingResult.data.insertId;
+        const booking_id = bookingResult.insertId;
 
         db.query(addBookingDetail, [booking_id, alamat, instagram, tujuan_sewa, rute, jaminan, total_price, tempat_ambil, start_time, end_time, start_date, end_date, 'Awaiting Payment', dateFormat, dateFormat], (err, detailResult) => {
           if (err) throw err;
-
-          response(201, detailResult, 'Successfully add new booking', res);
+          db.query(addPayment, [booking_id, 'Pending'], (err, paymentResult) => {
+            response(201, { bookingResult: detailResult, paymentResult: paymentResult }, 'Successfully add new booking', res);
+          });
         });
       });
-    } catch (err) {
-      response(500, {}, err.message, res);
-    }
-  },
-  addNewPayment: (req, res) => {
-    try {
-      const { booking_id, amount } = req.body;
-      const searchBookingId = 'select id from bookings where id = ?';
-
-      db.query(searchBookingId, [booking_id], (err, searchBookingResult) => {});
-      const sql = 'INSERT INTO payments (id, booking_id, amount, payment_date, payment_status) VALUES (?, ?, ?, ?, ?)';
-
-      // db.query(sql, [booking_id, amount, new Date().toISOString().slice(0, 19).replace('T', ' '), 'Pending'], (err, result) => {});
     } catch (err) {
       response(500, {}, err.message, res);
     }
